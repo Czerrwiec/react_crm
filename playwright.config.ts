d0e403e6c5b93de4c3cf64aa51e513b50ee23d1f
Tomasz <czerrwiec@gmail.com>
2026-02-18 08:03:21 +0100

    Playwright config

---
+++ playwright.config.ts
@@ -0,0 +1,41 @@
+import { defineConfig, devices } from '@playwright/test';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
+export default defineConfig({
+  testDir: './e2e',
+  timeout: 30000,
+  
+  fullyParallel: true,
+  
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  outputDir: './e2e/report/test-results',
+  
+  reporter: [
+    ['html', { outputFolder: 'e2e/report' }],
+    ['junit', { outputFile: 'e2e/report/test-results/junit.xml' }],
+    ['list'], 
+  ],
+  
+  use: {
+    baseURL: process.env.PLAYWRIGHT_BASE_URL || 'https://easy-drive-test.vercel.app',
+    extraHTTPHeaders: {
+      'x-vercel-protection-bypass': process.env.VERCEL_AUTOMATION_BYPASS_SECRET || '',
+    },
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    actionTimeout: 10000,
+    navigationTimeout: 30000,
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
