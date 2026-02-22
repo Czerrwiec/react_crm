f1b20a0ce8be82330f1fd110caa2c0b542e28ccb
Tomasz <czerrwiec@gmail.com>
2026-02-22 07:42:25 +0100

    Changed in POM and e2e

--- e2e/pages/base.pom.ts
+++ e2e/pages/base.pom.ts
@@ -4,7 +4,24 @@ const BaseSelectors = {
     emailInput: 'login-email-input',
     passwordInput: 'login-password-input',
     loginButton: 'login-button',
-};
+    recoveryButton: 'password-recovery-button',
+    recoveryEmail: 'recovery-email',
+    sendLinkButton: 'send-link-button',
+    backToLogin: 'back-login-button-sended',
+    loginError: 'login-error',
+    logoutButton: 'logout-button',
+    nav: {
+        dashboard: 'nav-dashboard',
+        students: 'nav-students',
+        calendar: 'nav-calendar',
+        cars: 'nav-cars',
+        instructors: 'nav-instructors',
+        packages: 'nav-packages',
+        settings: 'nav-settings'
+    }
+}
+
+type NavItem = keyof typeof BaseSelectors.nav;
 
 export class BasePom {
     constructor(private page: Page) { }
@@ -19,7 +36,54 @@ export class BasePom {
         await this.page.getByTestId(BaseSelectors.loginButton).click();
     }
 
+    async expectLoginError() {
+        await expect(
+            this.page.getByTestId(BaseSelectors.loginError)
+        ).toBeVisible();
+
+        await expect(
+            this.page.getByTestId(BaseSelectors.loginButton)
+        ).toBeVisible();
+    }
+
+    async goAndsendRecovery(email: string) {
+        await this.page.getByTestId(BaseSelectors.recoveryButton).click()
+        await this.page
+            .getByTestId(BaseSelectors.recoveryEmail)
+            .fill(email);
+
+        await Promise.all([
+            this.page.waitForResponse(res =>
+                res.url().includes('/auth/v1/recover') &&
+                res.request().method() === 'POST' &&
+                res.status() === 200
+            ),
+            this.page.getByTestId(BaseSelectors.sendLinkButton).click(),
+        ]);
+    }
+
+    async backToLogin() {
+        await this.page.getByTestId(BaseSelectors.backToLogin).click();
+    }
+
+    async logOut() {
+        await this.page.getByTestId(BaseSelectors.logoutButton).click();
+    }
+
     async expectRedirectTo(path: string) {
         await expect(this.page).toHaveURL(new RegExp(path));
     }
+
+    private navLocator(item: NavItem) {
+        return this.page.getByTestId(BaseSelectors.nav[item]);
+    }
+
+    async goToNavAndConfirmActiveTab(item: NavItem) {
+        const nav = this.navLocator(item);
+
+        await nav.click();
+        await expect(nav.locator('div'))
+            .toHaveClass(/text-primary-foreground/);
+    }
 }
+
